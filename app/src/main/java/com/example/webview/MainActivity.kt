d62ba7e2b8bb4edb333c62db34236ac6003ef914
Hajaniaina Rafaliarintsoa <hajaniaina.rafaliarintsoa@universign.com>
2023-01-25 15:26:37 +0100

    first commit

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the WebView element in the layout
+        // Find the WebView element in the layout
+        val webView = findViewById<WebView>(R.id.w_view)
+
+        // Enable JavaScript
+
+        // Enable JavaScript
+        webView.settings.javaScriptEnabled = true
+
+        // Set a WebViewClient to handle page navigation
+
+        // Set a WebViewClient to handle page navigation
+        webView.webViewClient = WebViewClient()
+
+        webView.clearCache(true)
+
+        // Load the desired URL
+
+        // Load the desired URL
+        webView.loadUrl("https://google.com")
+    }
+}
